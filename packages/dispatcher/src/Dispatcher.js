defeb54e9f796bdc9e0871498f1c4509968b3da1
fi11 <pavelsilinw@gmail.com>
2017-08-25 14:53:54 +0300

    add reverse for dispatcher

--- packages/dispatcher/src/Dispatcher.js
+++ packages/dispatcher/src/Dispatcher.js
@@ -1,17 +1,43 @@
 export default class Dispatcher {
-  constructor() {
+  constructor({ reverse } = {}) {
     this._listeners = [];
+    this._reverse = reverse;
+    this._callbackCount = 0;
   }
 
   register(callback) {
     this._listeners.push(callback);
+    this._callbackCount = this._listeners.length;
 
     return () => {
       this._listeners = this._listeners.filter(c => c !== callback);
+      this._callbackCount = this._listeners.length;
+    };
+  }
+
+  _callCallback(idx, payload) {
+    const cb = this._listeners[idx];
+
+    if (typeof cb === 'function') {
+      cb(payload);
     }
   }
 
+  getSize() {
+    return this._callbackCount;
+  }
+
   dispatch(payload) {
-    this._listeners.forEach(callback => callback(payload));
+    const len = this._callbackCount;
+
+    if (this._reverse) {
+      for (let i = len - 1; i > -1; i--) {
+        this._callCallback(i, payload);
+      }
+    } else {
+      for (let i = 0; i < len; i++) {
+        this._callCallback(i, payload);
+      }
+    }
   }
 }
